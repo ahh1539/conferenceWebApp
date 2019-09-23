ed51727e7f205aa01a4dcaa1f413a85ec94316b2
unknown <ahh1539@rit.edu>
2019-09-22 20:03:07 -0500

    big changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Register } from '../register-form/register.service';
+import { Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -9,25 +10,33 @@ import { Register } from '../register-form/register.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router, private regService: Register) { }
-  username: string;
-  password: string;
-  tempusername;
-  temmppass;
+
+  loginForm = this.fb.group({
+    username: [null, Validators.required],
+    password: [null, Validators.required],
+  });
+
+  constructor(private router: Router, private regService: Register, private fb: FormBuilder) { }
+
+  username;
+  password;
+
+  // tempusername;
+  // temmppass;
 
   ngOnInit() {
     var values = this.regService.getUser();
     if (values != undefined){
       if (values[0] != undefined){
-        this.tempusername = values[0];
-        this.temmppass = values[1];
+        this.username = values[0];
+        this.password = values[1];
       }
     }
   }
 
   
   login(): void {
-    if (this.username == this.tempusername && this.password == this.temmppass) {
+    if (this.username == this.getUsername() && this.password == this.getPassword()) {
       this.router.navigate(["user"]);
     } else {
       alert("Invalid credentials");
@@ -37,4 +46,13 @@ export class LoginComponent implements OnInit {
   register() {
     this.router.navigate(["register"])
   }
+
+  getPassword(){
+    return this.loginForm.get('password').value;
+  }
+
+  getUsername(){
+    return this.loginForm.get('username').value;
+  }
+
 }
--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -11,95 +11,42 @@ import { Register } from './register.service';
 export class RegisterFormComponent {
   addressForm = this.fb.group({
     company: null,
+    username: [null, Validators.required],
+    password: [null, Validators.required],
     firstName: [null, Validators.required],
     lastName: [null, Validators.required],
     address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
+    email: [null, Validators.required],
+    date: [null, Validators.required],
+    status: [null, Validators.required],
     postalCode: [null, Validators.compose([
       Validators.required, Validators.minLength(5), Validators.maxLength(5)])
     ],
-    shipping: ['free', Validators.required]
+    payment: ['credit/debit', Validators.required]
   });
 
   hasUnitNumber = false;
 
   states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
+    {name: 'Graduate Student', abbreviation: 'g'},
+    {name: 'Undergraduate Student', abbreviation: 'u'},
+    {name: 'Professor', abbreviation: 'p'},
+    {name: 'Industry', abbreviation: 'i'},
   ];
 
   constructor(private fb: FormBuilder, private router: Router, private regService: Register) {}
 
   onSubmit() {
-    alert(this.getPassword() + this.getUsername());
     this.regService.setUser(this.getUsername(), this.getPassword());
     this.router.navigateByUrl('/');
   }
 
   getPassword(){
-    return this.addressForm.get('company').value;
+    return this.addressForm.get('password').value;
   }
 
   getUsername(){
-    return this.addressForm.get('firstName').value;
+    return this.addressForm.get('username').value;
   }
 }
+
